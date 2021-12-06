c798718584df8029487070b4e7775981e81374a4
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-12-06 14:02:49 -0800

    feat: Report ranges for diagnostics

--- src/utils/stylelint/warning-to-diagnostic.ts
+++ src/utils/stylelint/warning-to-diagnostic.ts
@@ -46,7 +46,10 @@ export function warningToDiagnostic(
 	rules?: { [name: string]: stylelint.Rule },
 ): Diagnostic {
 	const start = Position.create(warning.line - 1, warning.column - 1);
-	const end = Position.create(warning.line - 1, warning.column);
+	const end =
+		typeof warning.endLine === 'number' && typeof warning.endColumn === 'number'
+			? Position.create(warning.endLine - 1, warning.endColumn - 1)
+			: Position.create(warning.line - 1, warning.column);
 
 	const ruleDocUrl =
 		rules?.[warning.rule] && `https://stylelint.io/user-guide/rules/${warning.rule}`;
